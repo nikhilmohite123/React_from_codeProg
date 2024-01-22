258e9ada732d6383e163c8734b4b79ff2ecdc704
Nikhil Mohite <mohitenikhil888@gmail.com>
2024-01-22 12:01:47 +0530

    basic of react and without react,vite how React is work

---
+++ 1) using_create_ReactElement/index.js
@@ -0,0 +1,22 @@
+
+// 1)
+// const el=React.createElement('h1',{className:'hey'},'hellow');
+// // it is create object =el
+
+// ReactDOM.createRoot(document.querySelector('#root')).render(el)
+
+
+
+
+// 2)
+// ReactDOM.createRoot(document.querySelector('#root')).render(<h1>hi nikhil</h1>)
+
+
+// 3)
+// function Hello(){
+//     return React.createElement('h1',{className:'hey'},'hellow')
+// }
+
+// ReactDOM.createRoot(document.querySelector('#root')).render(<Hello/>)
+                                                        // component shoulb be in capital ...beacuse render able to understand btw component and html element
+
