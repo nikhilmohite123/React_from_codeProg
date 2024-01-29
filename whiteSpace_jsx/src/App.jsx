f492c633e38fa50cdeed31d2adbde2818d72bf5d
Nikhil Mohite <mohitenikhil888@gmail.com>
2024-01-29 17:51:48 +0530

    Whitespace in jsx

---
+++ whiteSpace_jsx/src/App.jsx
@@ -0,0 +1,14 @@
+import WhieSpace from "./WhieSpace"
+
+
+function App() {
+ 
+
+  return (
+    <>
+    <WhieSpace name="nikhil" />
+    </>
+  )
+}
+
+export default App
---
+++ whiteSpace_jsx/src/WhieSpace.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function WhieSpace({name}) {
+  return (
+    <div >hi my{" "}<span style={{ fontFamily: "arial", color: "purple" }}>{name}</span> {" "}mohite <span>Gauri</span></div>
+  )
+}
+
+export default WhieSpace
